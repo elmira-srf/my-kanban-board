b2ca8792ceb69d67da31f25d74da35b698e0af21
Elmira <elmira.sarrafi@gmail.com>
2023-03-06 09:12:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 
 interface Task {
   id: string;
@@ -38,9 +38,9 @@ const columns: Column[] = [
 ];
 
 const Board = () => {
-  const [state, setState] = useState({ tasks, columns });
+const [state, setState] = useState<{ tasks: Task[]; columns: Column[] }>({ tasks, columns });
 
-  const onDragEnd = (result: any) => {
+  const onDragEnd = (result: DropResult) => {
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -50,30 +50,44 @@ const Board = () => {
     if (destination.droppableId === source.droppableId && destination.index === source.index) {
       return;
     }
-    
-    const column = state.columns.find((col) => col.id === source.droppableId);
-    const newTaskIds = column ? [...column.taskIds] : [];
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const sourceColumn = state.columns.find((col) => col.id === source.droppableId);
+    const destinationColumn = state.columns.find((col) => col.id === destination.droppableId);
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
+    if (!sourceColumn || !destinationColumn) {
+      return;
+    }
+
+    const sourceTaskIds = [...sourceColumn.taskIds];
+    const destinationTaskIds = [...destinationColumn.taskIds];
+
+    sourceTaskIds.splice(source.index, 1);
+    destinationTaskIds.splice(destination.index, 0, draggableId);
+
+    const newSourceColumn = {
+      ...sourceColumn,
+      taskIds: sourceTaskIds,
     };
-    type Column = {
-      id: string;
-      title: string;
-      taskIds: string[];
+
+    const newDestinationColumn = {
+      ...destinationColumn,
+      taskIds: destinationTaskIds,
     };
+
+    const newColumns = state.columns.map((col) => {
+      if (col.id === newSourceColumn.id) {
+        return newSourceColumn;
+      } else if (col.id === newDestinationColumn.id) {
+        return newDestinationColumn;
+      } else {
+        return col;
+      }
+    });
+
     const newState = {
       tasks: state.tasks,
-      columns: [
-
-        { id: 'column-1', title: 'To do', taskIds: ['task-1', 'task-2', 'task-3'] },
-        { id: 'column-2', title: 'In progress', taskIds: [] },
-        { id: 'column-3', title: 'Done', taskIds: [] },
-      ],
+      columns: newColumns,
     };
+
     setState(newState);
   };
 
@@ -93,7 +107,6 @@ const Board = () => {
                   const task = state.tasks.find((t) => t.id === taskId);
 
                   return (
-                    // <Draggable key={task?.id} draggableId={task.id} index={index}>
                     <Draggable key={task?.id} draggableId={task?.id ?? 'unknown'} index={index}>
 
                       {(provided, snapshot) => (
